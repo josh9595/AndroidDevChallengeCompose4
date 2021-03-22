cd78aa6022e453145d057f44fc02a883d8a09b97
josh9595 <josh.everett@bbc.co.uk>
2021-03-22 18:52:23 +0000

    Update icons

--- app/src/main/java/com/example/androiddevchallenge/util/AccessibilityUtils.kt
+++ app/src/main/java/com/example/androiddevchallenge/util/AccessibilityUtils.kt
@@ -18,6 +18,8 @@ package com.example.androiddevchallenge.util
 import android.content.Context
 import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.data.Forecast
+import com.example.androiddevchallenge.data.ForecastHour
+import com.example.androiddevchallenge.data.Weather
 
 class AccessibilityUtils(private val context: Context) {
     fun buildForecastOverview(
@@ -31,14 +33,29 @@ class AccessibilityUtils(private val context: Context) {
             forecast.overview.tempLow.toString()
         )
         val weatherString = context.getString(R.string.a11y_forecast_weather, forecast.overview.weather.text)
-        val weatherExtraString = calculateWeatherExtraString()
+        val weatherExtraString = calculateWeatherExtraString(forecast.hours)
 
         return "$temperatureString $weatherString $weatherExtraString"
     }
 
     fun intToDegrees(temp: Int): String = context.getString(R.string.a11y_forecast_degrees, temp.toString())
 
-    private fun calculateWeatherExtraString(): String {
+    private fun calculateWeatherExtraString(overviewForecast: String, hours: List<ForecastHour>): String {
+        var list = mutableListOf(
+            Pair(Weather.CLEAR, 0),
+            Pair(Weather.LIGHT_CLOUD, 0),
+            Pair(Weather.HEAVY_CLOUD, 0),
+            Pair(Weather.SHOWERS, 0),
+            Pair(Weather.LIGHT_RAIN, 0),
+            Pair(Weather.HEAVY_RAIN, 0),
+            Pair(Weather.THUNDERSTORM, 0),
+            Pair(Weather.HAIL, 0),
+            Pair(Weather.SLEET, 0),
+            Pair(Weather.SNOW, 0)
+        )
+//        hours.forEach { hour ->
+//            list.find { it.first ==  hour.weather }.second++
+//        }
         return context.getString(R.string.a11y_forecast_weather_same)
     }
 }
